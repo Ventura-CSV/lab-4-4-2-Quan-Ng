74c26c2df28b9f842e302d489d55fefa92c268b4
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-27 06:13:28 -0700

    LAB4.4.2: DONE

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    while True:
+        try:
+            number = int(input('Enter a Number: '))
+        except ValueError:
+            print ('Invalid Input: Value Must be Numeric')
+        else:
+            print (number)
+            break
+                
+    
     ########################################
     # Do not delete the return statement
     ########################################
